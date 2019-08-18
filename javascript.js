125a048af2eff1d877351332a76935058990e9b3
ZogeMung <1533567830@qq.com>
2019-08-18 12:31:43 +0800

    Add files via upload

--- javascript.js
+++ javascript.js
@@ -1,5 +1,76 @@
 $(function(){
-  $('.container1').on('click',function(){
-    window.location.href = "用“心”去体味生活.docx";
+
+  $('.item1 p:eq(-9)').on('click',function(){
+    window.location.href = "yuwen/《廉颇蔺相如列传》.ppt";
   });
+  $('.item1 p:eq(-8)').on('click',function(){
+    window.location.href = "yuwen/游褒禅山记.ppt";
+  });
+  $('.item1 p:eq(-7)').on('click',function(){
+    window.location.href = "yuwen/《兰亭集序》.ppt";
+  });
+  $('.item1 p:eq(-6)').on('click',function(){
+    window.location.href = "yuwen/张衡传.ppt";
+  });
+  $('.item1 p:eq(-5)').on('click',function(){
+    window.location.href = "yuwen/《苏武传》ppt.ppt";
+  });
+  $('.item1 p:eq(-4)').on('click',function(){
+    window.location.href = "yuwen/《赤壁赋》.ppt";
+  });
+  $('.item1 p:eq(-3)').on('click',function(){
+    window.location.href = "yuwen/鸿门宴.pptx";
+  });
+  $('.item1 p:eq(-2)').on('click',function(){
+    window.location.href = "yuwen/荆轲刺秦王.ppt";
+  });
+  $('.item1 p:eq(-1)').on('click',function(){
+    window.location.href = "yuwen/《烛之武退秦师》.ppt";
+  });
+
+  $('.item2 p:eq(-4)').on('click',function(){
+    window.location.href = "shuxue/20190814.zip";
+  });
+  $('.item2 p:eq(-3)').on('click',function(){
+    window.location.href = "shuxue/20190813.zip";
+  });
+  $('.item2 p:eq(-2)').on('click',function(){
+    window.location.href = "shuxue/20190808.zip";
+  });
+  $('.item2 p:eq(-1)').on('click',function(){
+    window.location.href = "shuxue/20190806.zip";
+  });
+
+  $('.item3 p:eq(-3)').on('click',function(){
+    window.location.href = "yingyu/U22.pptx";
+  });
+  $('.item3 p:eq(-2)').on('click',function(){
+    window.location.href = "yingyu/U21.pptx";
+  });
+  $('.item3 p:eq(-1)').on('click',function(){
+    window.location.href = "yingyu/U21L3 (Clara).ppt";
+  });
+
+
+
+
+  $('.item6 p:eq(-6)').on('click',function(){
+    window.location.href = "shengwu/rao3.2细胞器系统内的分工合作.ppt";
+  });
+  $('.item6 p:eq(-5)').on('click',function(){
+    window.location.href = "shengwu/第2单元 第1讲　细胞膜与细胞核.ppt";
+  });
+  $('.item6 p:eq(-4)').on('click',function(){
+    window.location.href = "shengwu/rao第1单元 第3讲　蛋白质和核酸.ppt";
+  });
+  $('.item6 p:eq(-3)').on('click',function(){
+    window.location.href = "shengwu/第1单元 第2讲　细胞中无机物、糖类和脂质.ppt";
+  });
+  $('.item6 p:eq(-2)').on('click',function(){
+    window.location.href = "shengwu/8月6日上课.pptx";
+  });
+  $('.item6 p:eq(-1)').on('click',function(){
+    window.location.href = "shengwu/rao补充20届高三一轮复习1.1.pptx";
+  });
+
 });
