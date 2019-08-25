ec9fe70f78b1db2df8ef03a7715e2ab84e89008b
ZogeMung <1533567830@qq.com>
2019-08-25 11:47:10 +0800

    Add files via upload

---
+++ view.js
@@ -0,0 +1,54 @@
+$(function(){
+
+  $('.item2 p:eq(-6)').on('click',function(){
+    window.location.href = "sx/20190824/";
+  });
+  $('.item2 p:eq(-5)').on('click',function(){
+    window.location.href = "sx/20190819/";
+  });
+  $('.item2 p:eq(-4)').on('click',function(){
+    window.location.href = "sx/20190814/";
+  });
+  $('.item2 p:eq(-3)').on('click',function(){
+    window.location.href = "sx/20190813/";
+  });
+  $('.item2 p:eq(-2)').on('click',function(){
+    window.location.href = "sx/20190808/";
+  });
+  $('.item2 p:eq(-1)').on('click',function(){
+    window.location.href = "sx/20190806/";
+  });
+
+
+  $('.item6 p:eq(-10)').on('click',function(){
+    window.location.href = "sw/rao第3单元第2讲ATP与细胞呼吸";
+  });
+  $('.item6 p:eq(-9)').on('click',function(){
+    window.location.href = "sw/rao第3单元第1讲降低化学反应活化能的酶";
+  });
+  $('.item6 p:eq(-8)').on('click',function(){
+    window.location.href = "sw/第2单元第3讲细胞的物质输入和输出";
+  });
+  $('.item6 p:eq(-7)').on('click',function(){
+    window.location.href = "sw/rao2020届高三上学期生物一轮复习《必修一3.2-细胞器与生物膜系统》(课件)(PPT)";
+  });
+  $('.item6 p:eq(-6)').on('click',function(){
+    window.location.href = "sw/rao3.2细胞器系统内的分工合作";
+  });
+  $('.item6 p:eq(-5)').on('click',function(){
+    window.location.href = "sw/第2单元第1讲细胞膜与细胞核";
+  });
+  $('.item6 p:eq(-4)').on('click',function(){
+    window.location.href = "sw/rao第1单元第3讲蛋白质和核酸";
+  });
+  $('.item6 p:eq(-3)').on('click',function(){
+    window.location.href = "sw/第1单元第2讲细胞中无机物、糖类和脂质";
+  });
+  $('.item6 p:eq(-2)').on('click',function(){
+    window.location.href = "sw/8月6日上课";
+  });
+  $('.item6 p:eq(-1)').on('click',function(){
+    window.location.href = "sw/rao补充20届高三一轮复习1.1";
+  });
+
+});
