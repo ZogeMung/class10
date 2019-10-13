d2da2327e5dea1f46f42d8c898ad486ae48b68a6
ZogeMung <1533567830@qq.com>
2019-10-13 14:06:10 +0800

    Add files via upload

--- home.js
+++ home.js
@@ -1,4 +1,7 @@
 var $$ = mdui.JQ;
+$$('body').on('loaded', function () {
+  mdui.snackbar('这里是高三10班');
+});
 $$('#ClassSong').on('opened.mdui.dialog', function () {
   $$('#ClassSongmp3')[0].play();
 });
