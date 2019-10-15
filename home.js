2c5abfbb4d1f376ce9785c261fe959196b1663a7
ZogeMung <1533567830@qq.com>
2019-10-15 18:09:48 +0800

    Update home.js

--- home.js
+++ home.js
@@ -5,3 +5,4 @@ $$('#ClassSong').on('opened.mdui.dialog', function () {
 $$('#ClassSong').on('close.mdui.dialog', function () {
   $$('#ClassSongmp3')[0].pause();
 });
+mdui.snackbar({message: '高三10班 (Champion)', timeout: 1000});
